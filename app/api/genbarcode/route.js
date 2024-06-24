57673f9489f5af5f75183df2d32834fdbe90ece7
Numan Bin Nasrullah <numanbinnasrullah@gmail.com>
2024-06-24 12:07:08 +0500

    ok3

--- app/api/genbarcode/route.js
+++ app/api/genbarcode/route.js
@@ -3,17 +3,17 @@ import { Connect } from "../config/dbase";
 import { Orders } from "../models/ordersdata";
 
 export async function POST(get) {
-  Connect();
-  const data = await get.json();
-  console.log("Server Data", data);
-  // return NextResponse.json({ msg: "Sb OK" }, { status: 200 });
-  try {
+  // Connect();
+  // const data = await get.json();
+  // console.log("Server Data", data);
+  return NextResponse.json({ msg: "Sb OK" }, { status: 200 });
+  // try {
     
-    console.log("sheet data upload showing server", data);
-    const savedata = await Orders.create(data);
-    console.log("Ssssssssssssssssssssss", savedata);
-    return NextResponse.json({ msg: "Sb OK", savedata }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ msg: "ERROR" }, { status: 500 });
-  }
+  //   console.log("sheet data upload showing server", data);
+  //   const savedata = await Orders.create(data);
+  //   console.log("Ssssssssssssssssssssss", savedata);
+  //   return NextResponse.json({ msg: "Sb OK", savedata }, { status: 200 });
+  // } catch (error) {
+  //   return NextResponse.json({ msg: "ERROR" }, { status: 500 });
+  // }
 }
